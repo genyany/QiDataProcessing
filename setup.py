a86d1678f80eeec23115d1799d5b9cee802c409c
carlsnow <carl.snow.china@gmail.com>
2019-11-18 18:47:28 +0800

    Some Change Add Doc string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='QiDataProcessing',
-    version='1.4.1',
+    version='1.4.2',
     description=(
         'read data from custom bytes file'
     ),
